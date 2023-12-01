bb56551c855e4f5c3d49f7e77d899ed9b6a2be4c
JjangSW <128954027+jjang6251@users.noreply.github.com>
2023-12-01 15:22:15 +0900

    console

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/signup', (req, res) => {
+    console.log(req.body);
     const saltRounds = 10;
     const password = req.body.password;
+
     bcrypt.hash(password, saltRounds, function(err, hashed_password){
-        if(err) throw err;
+        if(err) {
+            console.error('해시 생성 중 에러 발생:', err);
+            return res.status(500).send('회원가입 중 에러가 발생했습니다.');
+        }
+
         models.User.create({
             userid: req.body.id,
             password: hashed_password,
@@ -23,9 +29,16 @@ app.post('/signup', (req, res) => {
             location: req.body.location,
             nickname: req.body.nickname
         })
-    })
-    return res.status(200).send('회원가입 완료!');
-})
+        .then(() => {
+            return res.status(200).send('회원가입 완료!');
+        })
+        .catch((error) => {
+            console.error('DB에 회원가입 정보 저장 중 에러 발생:', error);
+            return res.status(500).send('회원가입 정보 저장 중 에러가 발생했습니다.');
+        });
+    });
+});
+
 
 
 app.listen(PORT, () => console.log(`${PORT} connected`));
